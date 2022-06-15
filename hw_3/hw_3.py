c155442602bb65c033dc6fdb865e0699579c211a
KyzukY <kyzuk.yr@gmail.com>
2022-06-15 20:08:33 +0300

    hw_3

--- hw_3/hw_3.py
+++ hw_3/hw_3.py
@@ -8,42 +8,105 @@
 #   >, < меньше или больше
 #   при вызове метода len() подсчитывать сумму сторон
 
-class Rectangle:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-        self.area = x * y
+# class Rectangle:
+#     def __init__(self, x: int, y: int):
+#         self.x = x
+#         self.y = y
+#         self.area = x * y
+#
+#     def __add__(self, other):
+#         return self.area + other.area
+#
+#     def __sub__(self, other):
+#         return self.area - other.area
+#
+#     def __eq__(self, other):
+#         return self.area == other.area
+#
+#     def __ne__(self, other):
+#         return self.area != other.area
+#
+#     def __lt__(self, other):
+#         return self.area < other.area
+#
+#     def __gt__(self, other):
+#         return self.area > other.area
+#
+#     def __len__(self):
+#         return self.x * 2 + self.y * 2
+#
+#
+# r1 = Rectangle(10, 10)
+# r2 = Rectangle(13, 13)
+#
+# print(r1 + r2)
+# print(r1 - r2)
+# print(r1 == r2)
+# print(r1 != r2)
+# print(r1 < r2)
+# print(r1 > r2)
+# print(len(r1))
+# print(len(r2))
+#
+# создать класс Human (name, age)
+# создать два класса Prince и Cinderella:
+# у золушки должно быть имя возраст и размер ноги
+# у принца имя, возраст и размер найденной туфельки, так же должен быть метод который принимает лист золушек и ищет ту самую
+#
+# в классе золушки должна быть переменная count которая будет считать сколько экземпляров класса золушка было создано
+# и метод класса который будет показывать это количество
 
-    def __add__(self, other):
-        return self.area + other.area
+class Human:
+    def __init__(self, name: str, age: int):
+        self.age = age
+        self.name = name
 
-    def __sub__(self, other):
-        return self.area - other.area
+    def __str__(self):
+        return self.name, self.age
 
-    def __eq__(self, other):
-        return self.area == other.area
-
-    def __ne__(self, other):
-        return self.area != other.area
-
-    def __lt__(self, other):
-        return self.area < other.area
-
-    def __gt__(self, other):
-        return self.area > other.area
-
-    def __len__(self):
-        return self.x * 2 + self.y * 2
+    def __repr__(self):
+        return self.__str__()
 
 
-r1 = Rectangle(10, 10)
-r2 = Rectangle(13, 13)
+class Cinderella(Human):
+    count = 0
 
-print(r1 + r2)
-print(r1 - r2)
-print(r1 == r2)
-print(r1 != r2)
-print(r1 < r2)
-print(r1 > r2)
-print(len(r1))
-print(len(r2))
+    @classmethod
+    def inc_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def get_count(cls):
+        return cls.count
+
+    def __init__(self, name: str, age: int, size: int):
+        super().__init__(name, age)
+        self.size = size
+        Cinderella.inc_count()
+
+    def __str__(self):
+        return f'{super().__str__()} ({self.size})'
+
+
+class Prince(Human):
+    def __init__(self, name: str, age: int, size: int):
+        super().__init__(name, age)
+        self.size = size
+
+    def __str__(self):
+        return f'{super().__str__()} ({self.size})'
+
+    def find(self, find_list: list[Cinderella]):
+        for cinderella in find_list:
+            if cinderella.size == self.size:
+                return cinderella
+        return None
+
+prince1 = Prince('Max', 25, 35)
+cinderellas_list = [Cinderella('Kira', 22, 34), Cinderella('Anna', 25, 35), Cinderella('Olya', 26, 36)]
+cinderella_find = prince1.find(cinderellas_list)
+
+
+print(prince1)
+print(cinderellas_list)
+print(cinderella_find)
